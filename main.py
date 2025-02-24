2b68ef24fad03f0fdc01ddcd3d40dddb6a05bd56
pobletecl <clemente.poblete@gmail.com>
2025-02-24 18:46:29 -0300

    separated main.py functions to main.py and stats.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from stats import count_words
 def main():
     count_words(get_book_text("books/frankenstein.txt"))
 
@@ -5,9 +6,5 @@ def get_book_text(file_path):
     with open(file_path) as f:
         file_contents = f.read()
         return file_contents
-
-def count_words(text):
-    num_words = len(text.split())
-    print(num_words, "words found in the document")
 
 main()
---
+++ stats.py
@@ -0,0 +1,3 @@
+def count_words(text):
+    num_words = len(text.split())
+    print(num_words, "words found in the document")
