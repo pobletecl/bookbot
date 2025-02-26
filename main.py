8c7b7d3dc1152489d2eaec327c20c5ad328cc7bb
pobletecl <clemente.poblete@gmail.com>
2025-02-25 21:22:00 -0300

    removed hard coded text references and made it possible to analyze different texts

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from stats import count_words
 from stats import count_characters
+import sys
 
 def main():
-    count_words(get_book_text("books/frankenstein.txt"))
-    count_characters(get_book_text("books/frankenstein.txt"))
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    elif len(sys.argv) == 2:
+        print("============ BOOKBOT ============")
+        print("Analyzing book found at " + str(sys.argv[1]) + "...")
+        count_words(get_book_text(sys.argv[1]))
+        count_characters(get_book_text(sys.argv[1]))
 
 def get_book_text(file_path):
     with open(file_path) as f:
--- stats.py
+++ stats.py
@@ -1,15 +1,21 @@
 def count_words(text):
     num_words = len(text.split())
-    print(num_words, "words found in the document")
+    print("----------- Word Count ----------")
+    print("Found", num_words, "total words")
 
 def count_characters(text):
+    print("--------- Character Count -------")
     char_dict = dict.fromkeys(list(text.lower()))
+
     for char in text.lower():
         if char_dict[char] == None:
             char_dict[char] = 1
+            
         else:
             char_dict[char] += 1
-    
-    print(char_dict)
-    
+    del char_dict[" "]
+    sorted_list = {key: val for key, val in sorted(char_dict.items(), key = lambda ele: ele[1], reverse = True)}
+    for key in sorted_list:
+        print(key + ": " + str(sorted_list[key]))
 
+    print("============= END ===============")
